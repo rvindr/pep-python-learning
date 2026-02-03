27bad1c0900b55ad72fe85ff978beeabd8457aa2
Ravinder <rvindervrma@gmail.com>
2026-02-03 10:24:26 +0530

    exception

--- Day11/main.py
+++ Day11/main.py
@@ -8,3 +8,14 @@ try:
     print_num(-10)
 except ValueError as e:
     print("error: ",e)
+
+
+class AgeError:
+    pass
+
+def check_age(age):
+    if age < 0:
+        print('age never be negative')
+    print('Age is: ', age)
+
+check_age(-19)
